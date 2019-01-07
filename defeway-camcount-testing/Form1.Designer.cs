15a08a5b64e7c217c6f8d83036e7b8706c685287
jake hartley <puffleboy101@hotmail.co.uk>
2019-01-07 20:29:10 +0000

    --help me

--- defeway-camcount-testing/Form1.Designer.cs
+++ defeway-camcount-testing/Form1.Designer.cs
@@ -28,13 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(374, 511);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(294, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(374, 482);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(294, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "get";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(15, 25);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(190, 299);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Enabled = false;
+            this.richTextBox2.Location = new System.Drawing.Point(211, 25);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(190, 299);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Enabled = false;
+            this.richTextBox3.Location = new System.Drawing.Point(407, 25);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(190, 299);
+            this.richTextBox3.TabIndex = 4;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Enabled = false;
+            this.richTextBox4.Location = new System.Drawing.Point(603, 25);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(190, 299);
+            this.richTextBox4.TabIndex = 5;
+            this.richTextBox4.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "T1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "T2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(404, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "T3:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(600, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "T4:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 330);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "directory";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(96, 330);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "ips";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(177, 330);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "start";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(718, 330);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "about";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(810, 360);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "testing yeet";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- defeway-camcount-testing/Form1.cs
+++ defeway-camcount-testing/Form1.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +19,158 @@ namespace defeway_camcount_testing
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public string cookiePub;
+        public string camfinal;
+        public string pCamCount;
+        public string exepath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\img\";
+        string inputSafeFileName;
+        string inputFileName;
+        string[] lines;
+
+        public class MyWebClient : WebClient
+        {
+            protected override WebRequest GetWebRequest(Uri address)
+            {
+                var req = base.GetWebRequest(address);
+                req.Timeout = 5000;
+                return req;
+            }
+        }
+
+        async Task PutTaskDelay()
+        {
+            await Task.Delay(100);
+        }
+
+        public void TestThread(string[] uris)
+        {
+            foreach(string s in uris){
+                Download(s);
+            }
+        }
+
+        public async void Download(string uri)
+        {
+            string camcount;
+
+            try
+            {
+                camcount = await GetCamCount2(new Uri(uri));
+                int count = Int32.Parse(camcount);
+                for(int i = 0; i == count; i++)
+                {
+
+                }
+            }
+            catch
+            {
+                //v bad uri
+            }
+        }
+
+        public async Task<String> GetCamCount2(Uri uri)
+        {
+            //test if host is alive
+            WebRequest request = WebRequest.Create(uri);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response == null || response.StatusCode != HttpStatusCode.OK)
+            {
+                return ("BADURI");
+            }
+
+            WebBrowser w = new WebBrowser();
+            string cookie = "null";
+            w.Navigate(uri);
+            while (cookie == "null")
+            {
+                try
+                {
+                    cookie = w.Document.Cookie;
+                }
+                catch
+                {
+                    //was unable to get cookie
+                    cookie = "null";
+                }
+                cookie = w.Document.Cookie;
+                if (string.IsNullOrEmpty(cookie))
+                {
+                    cookie = "null";
+                }
+                await PutTaskDelay();
+
+                if(!(cookie == "null"))
+                {
+                    cookiePub = cookie;
+                    string[] cookieAr = cookie.Split(';');
+                    foreach(string s in cookieAr)
+                    {
+                        if (s.Contains("dvr_camcnt"))
+                        {
+                            pCamCount = s;
+                            pCamCount = pCamCount.Replace("dvr_camcnt", "");
+                            pCamCount = pCamCount.Replace("=", "");
+                            return (pCamCount);
+                        }
+                    }
+                    return ("idk");
+                }
+            }
+            return ("idk");
+        }
+        
+        public void Finalprint()
+        {
+            //Console.WriteLine("camfinal: " + cookiePub);
+            Console.WriteLine("\n\n" + pCamCount);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread a = new Thread(() => Download(textBox1.Text));
+            
+            Console.WriteLine(a.ThreadState);
+            a.IsBackground = true;
+            a.Start();
+            Console.WriteLine(a.ThreadState);
+
+            //Console.WriteLine(cookiePub);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    exepath = fbd.SelectedPath + "/";
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Input File";
+                dlg.Filter = "Text Files | *.txt";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    inputSafeFileName = dlg.SafeFileName;
+                    inputFileName = dlg.FileName;
+                    lines = System.IO.File.ReadAllLines(inputFileName);
+                }
+            }
         }
     }
 }
